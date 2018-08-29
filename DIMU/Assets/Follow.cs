a22ab032394b2a2c6be165287e44d178ea31a8dc
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2018-08-29 13:53:46 -0600

    initcomiit

---
+++ DIMU/Assets/Follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField] Transform Target;
+    [SerializeField] float VerticalMod = 2;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Target.position + Vector3.up * VerticalMod;
+	}
+}
