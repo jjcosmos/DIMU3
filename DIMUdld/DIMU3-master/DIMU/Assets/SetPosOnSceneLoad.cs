86c441ab6f75bbc3aff24495ba6534361e6c8633
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2018-08-29 22:39:18 -0600

    Modular Platforms & Camera control

---
+++ DIMUdld/DIMU3-master/DIMU/Assets/SetPosOnSceneLoad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosOnSceneLoad : MonoBehaviour {
+
+    [SerializeField] Transform startingPosition;
+	// Use this for initialization
+	void Start () {
+        transform.position = startingPosition.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
